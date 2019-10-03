739f94431f0ed5b75f39252852f473dda343215c
Jacob Harrington <jharrington4@gmail.com>
2019-10-03 09:07:40 -0700

    updated app.js

--- get-random-throw.js
+++ get-random-throw.js
@@ -1,5 +1,5 @@
 const getRandomThrow = () => {
-    const randomNumber = Math.floor((Math.randomNumber() * 2) + 1);
+    const randomNumber = Math.floor((Math.random() * 3) + 1);
     if (randomNumber === 1){
         return 'rock';
     } else if (randomNumber === 2){
--- r-p-s/r-p-s.js
+++ r-p-s/r-p-s.js
@@ -1,13 +1,22 @@
 // Initializing DOM State
 import checkResults from '../check-result.js';
 import getRandomThrow from '../get-random-throw.js';
+const myButton = document.getElementById('button');
 
 
 
 // changing state
+const makeSelection = () => {
+    const userInput = document.querySelector('input:checked');
+    const userInputValue = userInput.value;
+    const computerValue = getRandomThrow();
+    const winOrLose = checkResults(userInputValue, computerValue);
+    console.log(winOrLose);
 
+    
+}
 
 
 
 // event listeners
-
+myButton.addEventListener('click', makeSelection);
