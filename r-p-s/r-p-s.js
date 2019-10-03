920aa557b8e874fef09a8272273ba2f83d1ab637
Jacob Harrington <jharrington4@gmail.com>
2019-10-02 17:45:01 -0700

    created r-p-s html, js, and css. Updated index for nav to r-p-s

---
+++ r-p-s/r-p-s.js
@@ -0,0 +1,13 @@
+// Initializing DOM State
+import checkResults from '../check-result.js';
+import getRandomThrow from '../get-random-throw.js';
+
+
+
+// changing state
+
+
+
+
+// event listeners
+
